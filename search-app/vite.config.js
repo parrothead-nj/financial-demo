36ea7eb98287c2f6efff5347b5d9c63782d088d3
Kevin K <kevin.m.klepp@gmail.com>
2026-02-03 16:43:00 -0500

    Deployment of Vue Search Interface

---
+++ search-app/vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path' // Added this import
+
+// Define __dirname for ES Modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  base: '/financial-demo/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      // Points directly to the SearchStax theme in your node_modules
+      '@searchstax-theme': path.resolve(__dirname, 'node_modules/@searchstax-inc/searchstudio-ux-vue/dist/styles/mainTheme.css')
+    },
+  },
+})
